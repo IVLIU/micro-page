7949e00f0be0ddba1e045388753cac4b61f2c551
鹈鹕 <tihu@myweimai.com>
2020-06-30 14:26:49 +0800

    feat: npm publish

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,7 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import MicroPage from '../.';
+import MicroPage from '../src';
 import useSingleHistory from '../src/useSingleHistory';
 import Comp from './Comp';
 
